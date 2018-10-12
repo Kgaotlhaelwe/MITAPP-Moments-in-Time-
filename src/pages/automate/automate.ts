4edd428035df4b63680b17c46d259328d0ca91c2
Kabelo Gaotlhaelwe <Kgaotlhaelwe@gmail.com>
2018-10-12 14:01:08 +0200

    Friday 14:02

--- src/pages/automate/automate.ts
+++ src/pages/automate/automate.ts
@@ -20,23 +20,23 @@ import { DatabaseProvider } from '../../providers/database/database';
   templateUrl: 'automate.html',
 })
 export class AutomatePage {
-  phoneNumber ;
- // name ;
+  phoneNumber;
+  // name ;
   peronalisedMsg
   //sms ;
   graduation = this.navParams.get("graduation")
-  message ;
+  message;
 
-  date = new Date() ;
+  date = new Date();
   countDownDate = this.navParams.get("countDownDate")
 
-  chosenDate =this.navParams.get("chosenDate");
-  chosenTime =this.navParams.get("chosenTime");
+  chosenDate = this.navParams.get("chosenDate");
+  chosenTime = this.navParams.get("chosenTime");
   name = this.navParams.get("name");
-  categoryChosen = this.navParams.get("categoryChosen") ;
+  categoryChosen = this.navParams.get("categoryChosen");
 
-  image ;
-  constructor(public navCtrl: NavController, public navParams: NavParams,public alertCtrl: AlertController,public actionSheetCtrl: ActionSheetController, private localNotifications: LocalNotifications,private sms:SMS, private contacts: Contacts, private db:DatabaseProvider ) {
+  image;
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController, public actionSheetCtrl: ActionSheetController, private localNotifications: LocalNotifications, private sms: SMS, private contacts: Contacts, private db: DatabaseProvider) {
   }
 
   ionViewDidLoad() {
@@ -44,109 +44,102 @@ export class AutomatePage {
 
     console.log(this.graduation);
 
-    if("Birthday" == this.categoryChosen){
-      this.image ="../../assets/icon/icons8_Wedding_Cake_100px.png";
+    if ("Birthday" == this.categoryChosen) {
+      this.image = "../../assets/icon/icons8_Wedding_Cake_100px.png";
     }
-    else if("Graduation"== this.categoryChosen ){
-      this.image ="../../assets/icon/icons8_Graduation_Cap_100px.png" ;
+    else if ("Graduation" == this.categoryChosen) {
+      this.image = "../../assets/icon/icons8_Graduation_Cap_100px.png";
 
-    }else if("Baby Shower" == this.categoryChosen ){
+    } else if ("Baby Shower" == this.categoryChosen) {
       this.image = "../../assets/icon/icons8_Pram_100px.png";
 
     }
-    else if("New Job" == this.categoryChosen ){
+    else if ("New Job" == this.categoryChosen) {
       this.image = "../../assets/icon/icons8_Briefcase_100px.png";
 
     }
-    else if("Anniversary" == this.categoryChosen ){
-      this.image ="../../assets/icon/icons8_Wedding_Gift_96px.png";
+    else if ("Anniversary" == this.categoryChosen) {
+      this.image = "../../assets/icon/icons8_Wedding_Gift_96px.png";
 
     }
-    else if("Wedding" == this.categoryChosen ){
+    else if ("Wedding" == this.categoryChosen) {
       this.image = "../../assets/icon/icons8_Diamond_Ring_100px.png";
 
     }
-    else if("Thinking of you" == this.categoryChosen ){
+    else if ("Thinking of you" == this.categoryChosen) {
       this.image = " ../../assets/icon/icons8_Collaboration_Female_Male_100px_1.png";
 
     }
-    else if("General" == this.categoryChosen ){
+    else if ("General" == this.categoryChosen) {
       this.image = "../../assets/icon/icons8_People_100px.png";
 
     }
   }
 
-  contactss(){
-    this.contacts.pickContact().then((data:any)=>{
-    console.log(data);
- 
-     this.phoneNumber=(data.phoneNumbers[0].value);
-   this.name=(data.displayName);
-   } , (error)=>{
-     alert(error)
-   })
+  contactss() {
+    this.contacts.pickContact().then((data: any) => {
+      console.log(data);
+
+      this.phoneNumber = (data.phoneNumbers[0].value);
+      this.name = (data.displayName);
+    }, (error) => {
+      alert(error)
+    })
   }
 
 
-  autoMessagessssss(message){
+  autoMessagessssss(message) {
 
-   let date = moment(this.chosenDate + " " + this.chosenTime).format('MMMM DD YYYY, h:mm:ss a');
-  // nnnnn
-     console.log(date);
-
-      // const alert = this.alertCtrl.create({
-      //       title: 'Confirmation',
-      //       subTitle: message,
-      //       buttons: ['OK']
-      //     });
-      //     alert.present();
-   
-    
+    let date = moment(this.chosenDate + " " + this.chosenTime).format('MMMM DD YYYY, h:mm:ss a');
 
 
- this.localNotifications.schedule({
-     text: this.chosenDate+ this.name,
-     trigger: {at: new Date(new Date(date) )} ,
- })
- 
-    this.db.saveSentMessages(this.name ,message, this.chosenDate, this.image).then(()=>{}, (error)=>{}) ;
-   
-    this.navCtrl.push(AboutPage,{message:message, countDownDate:this.countDownDate, name:this.name})
 
-}
-    showConfirm() {
-      const confirm = this.alertCtrl.create({
-        title: this.name,
-        message: this.phoneNumber,
-        buttons: [
-          {
-            text: 'Disagree',
-            handler: () => {
-              console.log('Disagree clicked');
-            }
-          },
-          {
-            text: 'Schedule',
-            handler: () => {
-              console.log(this.phoneNumber);
-           
-              this.sms.send(this.phoneNumber, this.message).then(()=>{
-                            alert('success')
-                          } , (error)=>{
-                            alert(error)
-              
-                         }) 
-   
-   
-            }
+    this.localNotifications.schedule({
+
+      title: "MOMENT IN TIME",
+      text: this.name,
+      icon: 'http://example.com/icon.png',
+      trigger: { at: new Date(new Date(date)) },
+    })
+
+    this.db.saveSentMessages(this.name, message, this.chosenDate, this.image).then(() => { }, (error) => { });
+
+    this.navCtrl.push(AboutPage, { message: message, countDownDate: this.countDownDate, name: this.name })
+
+  }
+  showConfirm() {
+    const confirm = this.alertCtrl.create({
+      title: this.name,
+      message: this.phoneNumber,
+      buttons: [
+        {
+          text: 'Disagree',
+          handler: () => {
+            console.log('Disagree clicked');
           }
-        ]
-      });
-      confirm.present();
-      
-    }
+        },
+        {
+          text: 'Schedule',
+          handler: () => {
+            console.log(this.phoneNumber);
+
+            this.sms.send(this.phoneNumber, this.message).then(() => {
+              alert('success')
+            }, (error) => {
+              alert(error)
+
+            })
 
 
-  
+          }
+        }
+      ]
+    });
+    confirm.present();
+
+  }
+
+
+
 
 }
